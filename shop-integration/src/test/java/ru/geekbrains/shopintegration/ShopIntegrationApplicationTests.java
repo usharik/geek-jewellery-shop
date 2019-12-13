88d9d224f8d624a91ee2fc8c857cdaf7e45a2451
usharik <usharik@mail.ru>
2019-12-13 17:07:53 +0100

    Lesson-7 Spring Integration example

---
+++ shop-integration/src/test/java/ru/geekbrains/shopintegration/ShopIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shopintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
